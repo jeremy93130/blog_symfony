a2f2736b3b0505797b1bf3fecf92a896e79324e0
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-10-17 16:48:16 +0200

    formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,18 +5,19 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $contact = new Contact();
 
         $form = $this->createForm(ContactType::class, $contact);
-        
+
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
             'contact' => $form->createView(),
