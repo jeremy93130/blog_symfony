fc84b4bb68acc88cf3591e91e71d99963b9426de
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-10-17 16:56:00 +0200

    Ajout methode pour recuperer les valeurs du formulaire et les envoyer vers la base de données

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,23 +4,29 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $contact = new Contact();
-
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+        }
+        // dd($contact);
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
-            'contact' => $form->createView(),
+            'contact' => $form->createView()
         ]);
     }
 }
